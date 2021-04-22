3d386eaa7447ba12cbda60587aa3a644a30c39db
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-04-21 20:36:33 -0600

    app.py render_template

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from flask_pymongo import PyMongo
 #Create an instance of Flask
 app=Flask(__name__)
 # Use flast_pymongo to set up mongo connection
-app.config["MONGO_URI"] ="mongodb://localhost:27017/classDB"
+app.config["MONGO_URI"] ="mongodb://localhost:27017/FullData"
 mongo=PyMongo(app)
 
 @app.route('/')
 def home():
-	mongo_data=mongo.db.brewery_14ers_db.find_one()
+	mongo_data=mongo.db.everything_14ers_db.find_one()
 	#print(list(data_from_mongo))	
 	if mongo_data:
-		return render_template('index.html', brewery_14ers_db=mongo_data)
+		return render_template('index.html', everything_14ers_db=mongo_data)
 	else:
 		return 'Error Try Again'
 	#return render_template('index.html', dict=data_from_mongo)
