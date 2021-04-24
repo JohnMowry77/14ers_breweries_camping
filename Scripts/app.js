8072c39c5b6a6f714112b29344d8e801e881993a
dalberghini <david.alberghini@gmail.com>
2021-04-24 10:08:47 -0600

    who cares at this point

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,24 +1,5 @@
 // // Chart Params
 // var svgWidth = 960;
-// var svgHeight = 500;
-
-// var margin = { top: 20, right: 40, bottom: 60, left: 50 };
-
-// var width = svgWidth - margin.left - margin.right;
-// var height = svgHeight - margin.top - margin.bottom;
-
-// // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-// var svg = d3
-//   .select("body")
-//   .append("svg")
-//   .attr("width", svgWidth)
-//   .attr("height", svgHeight);
-
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Import data from an external CSV file
-
 
 // function createFeatures(earthquakeData) {
 //   // Define a function we want to run once for each feature in the features array
@@ -81,10 +62,13 @@ d3.json("/Resources/mountains_db.json").then(function(data) {
   for(i=0; i<data.length; i++) {
     mt_longs.push(data[i]["Peak Longitude"]);
     mt_lats.push(data[i]["Peak Latitude"]);
-  
+    var mtIcon = L.icon({
+      iconUrl: 'mt.png',
+      iconSize: [20, 20]
+    });
   var marker = L.marker([mt_lats[i], mt_longs[i]], {
-  title: "My First Marker"
-}).addTo(myMap).bindPopup(`<br><b>Mountain Peak: ${data[i]["Mountain Peak"]}</b>	<br>Mountain Range: ${data[i]["Mountain Range"]}	<br>Elevation_ft: ${data[i]["Elevation_ft"]}	<br>fourteener: ${data[i]["fourteener"]}	<br>Prominence_ft: ${data[i]["Prominence_ft"]}	<br>Isolation_mi: ${data[i]["Isolation_mi"]}	<br>Peak Longitude: ${data[i]["Peak Longitude"]}	<br>Peak Latitude: ${data[i]["Peak Latitude"]}	<br>Standard Route: ${data[i]["Standard Route"]}	<br>Standard Route Distance (miles): ${data[i]["Standard Route Distance (miles)"]}	<br>Elevation Gain_ft: ${data[i]["Elevation Gain_ft"]}	<br>Difficulty: ${data[i]["Difficulty"]}	<br>Traffic Low: ${data[i]["Traffic Low"]}	<br>Traffic High: ${data[i]["Traffic High"]}	<br>photo: ${data[i]["photo"]}`);}});
+  icon : mtIcon
+}).addTo(myMap).bindPopup(`<br><b>Mountain Peak: ${data[i]["Mountain Peak"]}</b>	<br>Mountain Range: ${data[i]["Mountain Range"]}	<br>Elevation_ft: ${data[i]["Elevation_ft"]}	<br>fourteener: ${data[i]["fourteener"]}	<br>Prominence_ft: ${data[i]["Prominence_ft"]}	<br>Isolation_mi: ${data[i]["Isolation_mi"]}	<br>Peak Longitude: ${data[i]["Peak Longitude"]}	<br>Peak Latitude: ${data[i]["Peak Latitude"]}	<br>Standard Route: ${data[i]["Standard Route"]}	<br>Standard Route Distance (miles): ${data[i]["Standard Route Distance (miles)"]}	<br>Elevation Gain_ft: ${data[i]["Elevation Gain_ft"]}	<br>Difficulty: ${data[i]["Difficulty"]}	<br>Traffic Low: ${data[i]["Traffic Low"]}	<br>Traffic High: ${data[i]["Traffic High"]}`);}}); 
 
 
 
@@ -92,16 +76,38 @@ d3.json("/Resources/everything_14ers_db.json").then(function(data) {
   console.log(data)
 var camp_lats = []
 var camp_longs = []
+var beer_lats = []
+var beer_longs = []
 for(i=0; i<data.length; i++) {
-  camp_longs.push(data[i]["Campsite Longitude"]);
-  camp_lats.push(data[i]["Campsite Latitude"]);
-  var greenIcon = L.icon({
+  if("Campsite Latitude" in data[i]) {
+    camp_lats.push(data[i]["Campsite Latitude"]);
+  }
+  if("Campsite Longitude" in data[i]) {
+    camp_longs.push(data[i]["Campsite Longitude"]);
+  }
+
+  var campIcon = L.icon({
     iconUrl: 'camp.png',
     iconSize: [10, 10]
   });
-  var circle = L.marker([camp_lats[i], camp_longs[i]], {
-   icon: greenIcon}
-    ).addTo(myMap).bindPopup(`<br>${data[i]["name"]}</br>`)
+  L.marker([camp_lats[i], camp_longs[i]], {
+   icon: campIcon}
+    ).addTo(myMap).bindPopup(`<br><b>Campsite Name : ${data[i]["name"]}</b></br><br>Nearest 14er: ${data[i]["Mountain Peak"]}</br><br>Miles from 14er : ${data[i]["Distance from Campsite (mi)"]}</br>`)
+
+  // if("Brewery Latitudes" in data[i]) {
+  //   beer_lats.push(data[i]["Brewery Latitudes"]);
+  // }
+  // if("Brewery Longitudes" in data[i]) {
+  //   beer_longs.push(data[i]["Brewery Longitudes"]);
+  // }
+  // var beerIcon = L.icon({
+  //   iconUrl : "bottle.png",
+  //   iconSize : [10,10]
+  // });
+  // var beer = L.marker([beer_lats[i], beer_longs[i]], {
+  //   icon: beerIcon
+  // }).addTo(myMap).bindPopup(`<br><b>Brewery Name : ${data[i]["Brewery Name"]}</b></br><br>Brewery Address : ${data[i]["Brewery Address"]}</br><br>Brewery Website : ${data[i]["Brewery Website"]}</br><br>Nearest 14er : ${data[i]["Mountain Peak"]}</br>`)
+
   };
 });
 
